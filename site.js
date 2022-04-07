22c1bfb179babdd2d378f14bc31eea2b7176341d
Stan Smoczyk <stansmoczyk@gmail.com>
2022-04-07 14:36:49 -0400

    Win or lose logic installed

--- site.js
+++ site.js
@@ -1,15 +1,15 @@
 
-
+//Random picker for computer to play
 const computerPlay = () => {
     const randomNumber = (Math.floor(Math.random() * 3))
 if (randomNumber === 0){
-    return 'Rock';
+    return 'ROCK';
 }
 else if (randomNumber === 1){
-    return 'Paper';
+    return 'PAPER';
 }
 else if (randomNumber === 2){
-    return 'Scissors';
+    return 'SCISSORS';
 }
 }
 //console.log(computerPlay());
@@ -17,10 +17,41 @@ else if (randomNumber === 2){
 
 
 function playRound(playerSelection, computerSelection) {
-    //my code
-    console.log('Player chose: ' + playerSelection + ' Computer chose: ' + computerSelection);
+    let computerWins = 0;
+    let playerWins = 0;
+
+    for (let i =0; i <5; i++){
+        //code to play 5 rounds here
+        
+    }
+    console.log('Player chose: ' + playerSelection + ' Computer chose: ' + computerSelection);//Show it works for now
+}
+function comparePlay(playerSelection, computerSelection){
+
+        
+    if (playerSelection == computerSelection){
+        return ("It's a tie!");
+    }
+    if (playerSelection == 'ROCK'){
+        if (computerSelection == 'SCISSORS'){
+            return ("You win!");
+        }else{
+        return ("computer wins!");
+        }
+    }
+    if (playerSelection == "SCISSORS"){
+        if (computerSelection == "ROCK"){
+            return ("Computer wins!");
+        }else {
+            return("You win!");
+        }
+    }
 }
 
-const playerSelection = "rock";
+    
+
+
+const playerSelection = "ROCK";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
+console.log(comparePlay(playerSelection, computerSelection))
